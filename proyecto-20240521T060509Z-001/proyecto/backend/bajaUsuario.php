fbdddc1a3ba97a58064937f24cac601f57486766
GarzonMiguelA <152981166+GarzonMiguelA@users.noreply.github.com>
2024-05-23 08:31:24 +0000

    commit nav

--- proyecto-20240521T060509Z-001/proyecto/backend/bajaUsuario.php
+++ proyecto-20240521T060509Z-001/proyecto/backend/bajaUsuario.php
@@ -29,16 +29,27 @@ mysqli_close($conn);
         <div class="logo">CLUB BOX SOGACHE</div>
         <nav>
             <ul>
-                <li><a href="paginaPrincipal.html">Inicio</a></li>
+                <li><a href="index.html">Inicio</a></li>
                 <li><a href="horarios.html">Horarios</a></li>
-                <li><a href="resultados.html">Resultados</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Precios</a>
+                    <div class="dropdown-content">
+                        <a href="tarifa.html">Tarifas</a>
+                        <a href="entrenoPrivado.html">Entreno Privado</a>
+                    </div>
+                </li>
                 <li><a href="entrenadores.html">Entrenadores</a></li>
-                <li><a href="noticias.html">Noticias</a></li>
-                <li><a href="nutricional.html">Información Nutricional</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Blog</a>
+                    <div class="dropdown-content">
+                        <a href="nutricional.html">Información Nutricional</a>
+                        <a href="noticias.html">Noticias</a>
+                    </div>
+                </li>
                 <li><a href="sobreNosotros.html">Sobre Nosotros</a></li>
                 <li><a href="contacto.html">Contacto</a></li>
-                <li><a href="login.html">Iniciar Sesión</a></li>
-                <li><a href="SignUp.html">Crear Cuenta</a></li>
+                <li><a href="iniciarSesion.html">Iniciar Sesión</a></li>
+                <li><a href="registro.html">Crear Cuenta</a></li>
             </ul>
         </nav>
     </header>
