e7fc7baba719791e96fde71576b6627d0d8cd39b
= <=>
2024-05-27 09:48:56 +0200

    commit

--- proyecto-20240521T060509Z-001/proyecto/backend/pago.php
+++ proyecto-20240521T060509Z-001/proyecto/backend/pago.php
@@ -2,7 +2,7 @@
 session_start();
 include("connexio.php");
 
-// Verificar si el usuario está logueado
+
 if (!isset($_SESSION['email'])) {
     header("Location: iniciarSesion.html");
     exit();
@@ -11,27 +11,22 @@ if (!isset($_SESSION['email'])) {
 $email = $_SESSION['email'];
 $plan = $_POST['plan'];
 
-// Aquí se debería implementar la lógica de procesamiento del pago.
-// Por simplicidad, se asume que el pago fue exitoso.
+$ha_pagado = 1; 
 
-$ha_pagado = 1; // Suponiendo que el pago se realizó con éxito
-
-// Actualizar la base de datos para reflejar el pago
 $update_query = "UPDATE Persona SET ha_pagado = ? WHERE email = ?";
 $stmt = $conn->prepare($update_query);
 $stmt->bind_param("is", $ha_pagado, $email);
 $stmt->execute();
 
-// Notificar al entrenador
-$entrenador_email = "entrenador@example.com"; // Reemplaza con el email del entrenador
+
+$entrenador_email = "entrenador@example.com"; 
 $subject = "Nuevo pago realizado";
 $message = "El usuario con email $email ha contratado el plan $plan.";
 $headers = "From: no-reply@sogache.com";
 
-// Enviar el email
 mail($entrenador_email, $subject, $message, $headers);
 
-// Redirigir a una página de confirmación
+
 header("Location: confirmacionPago.html");
 exit();
 ?>
