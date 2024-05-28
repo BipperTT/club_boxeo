ca44af7ac2b186305c4feac76b139958b2906786
= <=>
2024-05-28 12:45:09 +0200

    cmmit

--- docs/enviar_respuesta.php
+++ docs/enviar_respuesta.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] !== 'entrenador') {
+if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] !== 'Entrenador') {
     header("Location: index.php");
     exit;
 }
