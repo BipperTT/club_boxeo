3c1e615e6be22a345c2ed9082ebcb056e4b18b53
= <=>
2024-05-28 09:31:59 +0200

    commit admin

--- api/logOut.php
+++ api/logOut.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-session_destroy();
-header("Location: ../docs/iniciarSesion.html");
+session_unset(); // Eliminar todas las variables de sesión
+session_destroy(); // Destruir la sesión
+header('Location: ../docs/index.php'); // Redirigir a la página de inicio (puede cambiar a la página de inicio de sesión si prefieres)
 exit();
 ?>
