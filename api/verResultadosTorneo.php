f035c8593484a5743a1963eecf7eda60c27edc3a
DanielSobrino14 <daniel.sobrino.7e7@itb.cat>
2024-05-30 10:20:23 +0200

    commit

--- api/verResultadosTorneo.php
+++ api/verResultadosTorneo.php
@@ -36,3 +36,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT' && isset($_GET['id'])) {
 }
 
 
+include('connexio.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $idEntrenador = $data['ID_entrenador'];
+    $diaSemana = $data['dia_semana'];
+    $horaInicio = $data['hora_inicio'];
+    $horaFinal = $data['hora_final'];
+    $id_usuario = $data['id_usuario'];
+
+    $query = "INSERT INTO Grupal (ID_entrenador, dia_semana, hora_inicio, hora_final, ID_usuario) VALUES ('$idEntrenador', '$diaSemana', '$horaInicio', '$horaFinal', '$id_usuario')";
+    
+    if (mysqli_query($conn, $query)) {
+        $response = array('status' => 'success');
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        $response = array('status' => 'error', 'error' => mysqli_error($conn));
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+
+
