b9d641581c28ee18bff2d0ad4bd0b8b241e51f10
DanielSobrino14 <daniel.sobrino.7e7@itb.cat>
2024-05-30 13:26:26 +0200

    commit

--- api/signUp.proc.php
+++ api/signUp.proc.php
@@ -2,11 +2,31 @@
 session_start();
 include("connexio.php");
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$password = $_POST['password'];
+$first_name = trim($_POST['first_name']);
+$last_name = trim($_POST['last_name']);
+$email = trim($_POST['email']);
+$phone = trim($_POST['phone']);
+$password = trim($_POST['password']);
+$confirm_password = trim($_POST['confirm_password']);
+
+if (empty($first_name) || empty($last_name) || empty($email) || empty($phone) || empty($password) || empty($confirm_password)) {
+    echo 'Por favor, rellena todos los campos.';
+    exit();
+}
+
+
+if ($password !== $confirm_password) {
+    echo 'Las contraseñas no coinciden.';
+    exit();
+}
+
+$sql_check_email = "SELECT * FROM Persona WHERE email = '$email'";
+$result_check_email = mysqli_query($conn, $sql_check_email);
+
+if (mysqli_num_rows($result_check_email) > 0) {
+    echo 'El correo electrónico ya está registrado.';
+    exit();
+}
 
 $sql_persona = "INSERT INTO Persona (nombre, apellido1, telefono, email, contraseña, tipo) 
                 VALUES ('$first_name', '$last_name', '$phone', '$email', '$password', 'usuario')";
